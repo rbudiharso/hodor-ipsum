a6453ee1dc0439470bf0bb3557d8f8bcc0634417
Rahmat Budiharso <rbudiharso@gmail.com>
2014-06-13 09:43:15 +0700

    use IIFE

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,52 +1,55 @@
 'use strict';
 
-var client = new ZeroClipboard($('.copy'));
-client.on( 'aftercopy', function( event ) {
-    $('body').append('<div class="alert alert-success fade in">Hodor copied to clipboard</div>');
-    $('.alert').show();
-    window.setTimeout(function () {
-        $('.alert').alert('close');
-    }, 1500);
-});
+(function (self) {
+    var client = new self.ZeroClipboard($('.copy'));
+    client.on( 'aftercopy', function () {
+        $('body').append('<div class="alert alert-success fade in">Hodor copied to clipboard</div>');
+        $('.alert').show();
+        window.setTimeout(function () {
+            $('.alert').alert('close');
+        }, 1500);
+    });
 
-function sample(hodorArray) {
-    var hodorIndex = Math.floor(Math.random() * hodorArray.length);
-    return hodorArray.splice(hodorIndex, 1)[0];
-}
-
-function hodorWord () {
-    return 'hodor';
-}
-
-function hodorSentence (numHodor) {
-    var hodors = [];
-    for (var i = 0; i < numHodor; i++) {
-        hodors.push(hodorWord());
+    function sample(hodorArray) {
+        var hodorIndex = Math.floor(Math.random() * hodorArray.length);
+        return hodorArray.splice(hodorIndex, 1)[0];
     }
-    hodors[0] = hodors[0][0].toUpperCase() + hodors[0].slice(1);
-    return hodors.join('  ') + '.';
-}
 
-function hodorParagraph (numHodor) {
-    var hodors = [];
-    for (var i = 0; i < numHodor; i++) {
-        var indexes = [10, 15, 20];
-        var randomNumber = sample(indexes);
-        hodors.push(hodorSentence(randomNumber));
+    function hodorWord () {
+        return 'hodor';
     }
-    return hodors.join(' ');
-}
 
-$('select').on('change', function () {
-    var hodors = [];
-    var numHodor = this.value || 3;
-    var randomNumber = sample([5, 8, 10]);
-
-    for (var i = 0; i < numHodor; i++) {
-        hodors.push(hodorParagraph(randomNumber));
+    function hodorSentence (numHodor) {
+        var hodors = [];
+        for (var i = 0; i < numHodor; i++) {
+            hodors.push(hodorWord());
+        }
+        hodors[0] = hodors[0][0].toUpperCase() + hodors[0].slice(1);
+        return hodors.join('  ') + '.';
     }
-    hodors = hodors.join('\n\n');
-    $('textarea').val(hodors);
-});
 
-$('textarea').val(hodorParagraph($('select').val()));
+    function hodorParagraph (numHodor) {
+        var hodors = [];
+        for (var i = 0; i < numHodor; i++) {
+            var randomNumber = sample([5, 7, 9]);
+            hodors.push(hodorSentence(randomNumber));
+        }
+        return hodors.join(' ');
+    }
+
+    function hodorIpsum (numHodor) {
+        var hodors = [];
+        for (var i = 0; i < numHodor; i++) {
+            var randomNumber = sample([4, 5, 6]);
+            hodors.push(hodorParagraph(randomNumber));
+        }
+        return hodors.join('\n\n');
+    }
+
+    $('select').on('change', function () {
+        var _hodorIpsum = hodorIpsum(this.value || 3);
+        $('textarea').val(_hodorIpsum);
+    });
+
+    $('textarea').val(hodorParagraph($('select').val()));
+})(window);
